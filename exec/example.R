753b46bb8154d2f631a5ba95073fa231d18d30ff
Trevor L. Davis <trevor.l.davis@stanford.edu>
2013-08-23 22:18:31 -0700

    Move LICENSE into code

--- exec/example.R
+++ exec/example.R
@@ -1,6 +1,19 @@
 #!/usr/bin/env Rscript
-# Note:  This example is a port of an example in the getopt package
-#        which is Copyright 2008 Allen Day
+# Copyright 2010-2013 Trevor L Davis <trevor.l.davis@stanford.edu>
+# Copyright 2008 Allen Day
+#  
+#  This file is free software: you may copy, redistribute and/or modify it  
+#  under the terms of the GNU General Public License as published by the  
+#  Free Software Foundation, either version 2 of the License, or (at your  
+#  option) any later version.  
+#  
+#  This file is distributed in the hope that it will be useful, but  
+#  WITHOUT ANY WARRANTY; without even the implied warranty of  
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  
+#  General Public License for more details.  
+#  
+#  You should have received a copy of the GNU General Public License  
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.  
 suppressPackageStartupMessages(library("optparse"))
 
 # specify our desired options in a list
--- inst/tests/test-optparse.R
+++ inst/tests/test-optparse.R
@@ -1,3 +1,17 @@
+# Copyright (c) 2010-2013 Trevor L Davis <trevor.l.davis@stanford.edu>
+#  
+#  This file is free software: you may copy, redistribute and/or modify it  
+#  under the terms of the GNU General Public License as published by the  
+#  Free Software Foundation, either version 2 of the License, or (at your  
+#  option) any later version.  
+#  
+#  This file is distributed in the hope that it will be useful, but  
+#  WITHOUT ANY WARRANTY; without even the implied warranty of  
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  
+#  General Public License for more details.  
+#  
+#  You should have received a copy of the GNU General Public License  
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.  
 option_list <- list( 
     make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
         help="Print extra output [default]"),
